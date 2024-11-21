f7c247f0aca28ae6fe1979cbb597aa3b359c58fa
rayhanfadhlan <rayhanfazka07@gmail.com>
2024-11-21 16:36:37 +0700

    fix: segment checksum

--- src/segment.cpp
+++ src/segment.cpp
@@ -48,14 +48,36 @@ Segment finAck(){
     return segment;
 }
 
-// update return type as needed
-uint16_t calculateChecksum(Segment segment){
+
+// return xor of all bits on the segment except checksum
+uint16_t calculateSum(Segment &segment){
     uint16_t new_sum = 0;
-    uint16_t* ptr= (uint16_t*)segment.payload;
-    for(uint16_t i = 0; i < 1460; i+=16){
+    new_sum ^= segment.sourcePort;
+    new_sum ^= segment.destPort;
+    new_sum ^= (uint16_t) (segment.seq_num);
+    new_sum ^= (uint16_t) (segment.seq_num>>16);
+    new_sum ^= (uint16_t) (segment.ack_num);
+    new_sum ^= (uint16_t) (segment.ack_num>>16);
+    new_sum ^= uint16_t(0) + segment.data_offset<<12 + segment.reserved<<8 + segment.flags.cwr<<7 + segment.flags.ece<<6 + segment.flags.urg<<5 + segment.flags.ack<<4 + segment.flags.psh<<3 + segment.flags.pst<<2 + segment.flags.syn<<1 + segment.flags.fin;
+    
+    new_sum ^= segment.urg_point;
+    new_sum ^= segment.window;
+    uint16_t* ptr= (uint16_t*)segment.options;
+    uint8_t length = sizeof(ptr)*8;
+    for(uint16_t i = 0; i < length; i+=16){
+        new_sum ^= *(ptr+i);
+    }    
+    ptr= (uint16_t*)segment.payload;
+    length = sizeof(ptr)*8;
+    for(uint16_t i = 0; i < length; i+=16){
         new_sum ^= *(ptr+i);
     }
-    return ~new_sum;
+    return new_sum;
+}
+
+// update return type as needed
+uint16_t calculateChecksum(Segment segment){
+    return ~calculateSum(segment);
 }
 
 /**
@@ -71,11 +93,7 @@ Segment updateChecksum(Segment segment){
  * Check if a TCP Segment has a valid checksum
  */
 bool isValidChecksum(Segment segment){
-    uint16_t valid_sum = 0;
-    uint16_t* ptr= (uint16_t*)segment.payload;
-    for(uint16_t i = 0; i < 1460; i+=16){
-        valid_sum ^= *(ptr+i);
-    }
+    uint16_t valid_sum = calculateSum(segment);
     valid_sum^=segment.checksum;
     return !(~valid_sum);
 }
