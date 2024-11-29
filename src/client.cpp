c8df6d58b25396a511a1a03498d59b118675905c
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-11-29 20:57:19 +0700

    fix: retry. goto don't work so we effort

--- src/client.cpp
+++ src/client.cpp
@@ -16,65 +16,71 @@ void Client::setServerTarget(string server_ip, int server_port) {
 void Client::run() {
     connection.listen();
 
-    // ======== Handshake ========
-    SegmentHandler segment_handler;
-    uint32_t initial_seq_num = segment_handler.generateInitialSeqNum();
-retry:
-    cout << YEL << "[i] [Handshake] [S=" << initial_seq_num << "] Sending SYN request to " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
-    Segment syn_segment = syn(initial_seq_num);
-    connection.send((char*)server_ip.c_str(), server_port, &syn_segment, MAXLINE);
+    bool retry = true;
+    while(retry) {
+        // ======== Handshake ========
+        SegmentHandler segment_handler;
+        uint32_t initial_seq_num = segment_handler.generateInitialSeqNum();
+        cout << YEL << "[i] [Handshake] [S=" << initial_seq_num << "] Sending SYN request to " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
+        Segment syn_segment = syn(initial_seq_num);
+        connection.send((char*)server_ip.c_str(), server_port, &syn_segment, MAXLINE);
 
-    sockaddr_in addr; socklen_t len;
-    Segment syn_ack_segment;
-    while(true) {
-        auto sync_ack_buffer_size = connection.recv(&syn_ack_segment, MAXLINE, &addr, &len);
-        if(sync_ack_buffer_size < 0) {
-            cout << RED << "[-] [Handshake] Timeout, retrying" << COLOR_RESET << endl;
-            goto retry;
+        sockaddr_in addr; socklen_t len;
+        Segment syn_ack_segment;
+        while(true) {
+            auto sync_ack_buffer_size = connection.recv(&syn_ack_segment, MAXLINE, &addr, &len);
+            if(sync_ack_buffer_size < 0) {
+                cout << RED << "[-] [Handshake] Timeout, retrying" << COLOR_RESET << endl;
+                retry = true;
+                break;
+            }
+            retry = false;
+            if(extract_flags(syn_ack_segment.flags) == SYN_ACK_FLAG && syn_ack_segment.seq_num == syn_segment.seq_num + 1) break;
         }
-        if(extract_flags(syn_ack_segment.flags) == SYN_ACK_FLAG && syn_ack_segment.seq_num == syn_segment.seq_num + 1) break;
+        if(retry) continue;
+
+        cout << BLU << "[+] [Handshake] [S=" << syn_ack_segment.seq_num << "] [A=" << syn_ack_segment.ack_num << "] Received SYN-ACK request from " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
+
+        Segment ack_segment = ack(syn_ack_segment.ack_num + 1);
+        cout << YEL << "[i] [Handshake] [A=" << ack_segment.ack_num << "] Sending ACK request to " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
+        connection.send((char*)server_ip.c_str(), server_port, &ack_segment, MAXLINE);
+
+
+        // ======== Established ========
+        cout << "[i] Ready to receive input from " << this->server_ip << ":" << server_port << endl;
+
+        cout << "[~] [Established] Waiting for segments to be sent" << endl;
+
+
+        char input_buffer[MAXLINE];
+        auto input_buffer_size = connection.recv(input_buffer, MAXLINE, &addr, &len);
+        cout << BLU << "[i] [Established] [Seg 1] [S=2266133600] ACKed" << COLOR_RESET << endl;
+        cout << BLU << "[i] [Established] [Seg 2] [S=2266135060] ACKed" << COLOR_RESET << endl;
+        cout << YEL << "[i] [Established] [Seg 1] [A=3165500900] Sent" << COLOR_RESET << endl;
+        cout << BLU << "[i] [Established] [Seg 3] [S=2266136520] ACKed" << COLOR_RESET << endl;
+        cout << YEL << "[i] [Established] [Seg 2] [A=3165502360] Sent" << COLOR_RESET << endl;
+        cout << YEL << "[i] [Established] [Seg 3] [A=3165503820] Sent" << COLOR_RESET << endl;
+
+        cout << "[~] [Established] Waiting for segments to be sent" << endl;
+
+
+        // ======== Closing ========
+        char fin_buffer[MAXLINE];
+        auto fin_buffer_size = connection.recv(fin_buffer, MAXLINE, &addr, &len);
+        cout << BLU << "[i] [Closing] Received FIN request from " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
+
+        cout << YEL << "[i] [Closing] Sending FIN-ACK request to " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
+        char fin_ack_buffer[MAXLINE]; ((char*)fin_ack_buffer)[0] = '\0';
+        connection.send((char*)server_ip.c_str(), server_port, (void*)fin_ack_buffer, MAXLINE);
+
+        char ack_buffer_closing[MAXLINE];
+        auto ack_buffer_closing_size = connection.recv(ack_buffer_closing, MAXLINE, &addr, &len);
+        cout << BLU << "[i] [Closing] Received FIN request from " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
+
+        cout << GRN << "[i] Connection closed successfully" << COLOR_RESET << endl;
+
+        handleMessage(input_buffer, input_buffer_size);
     }
-    cout << BLU << "[+] [Handshake] [S=" << syn_ack_segment.seq_num << "] [A=" << syn_ack_segment.ack_num << "] Received SYN-ACK request from " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
-
-    Segment ack_segment = ack(syn_ack_segment.ack_num + 1);
-    cout << YEL << "[i] [Handshake] [A=" << ack_segment.ack_num << "] Sending ACK request to " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
-    connection.send((char*)server_ip.c_str(), server_port, &ack_segment, MAXLINE);
-
-
-    // ======== Established ========
-    cout << "[i] Ready to receive input from " << this->server_ip << ":" << server_port << endl;
-
-    cout << "[~] [Established] Waiting for segments to be sent" << endl;
-
-
-    char input_buffer[MAXLINE];
-    auto input_buffer_size = connection.recv(input_buffer, MAXLINE, &addr, &len);
-    cout << BLU << "[i] [Established] [Seg 1] [S=2266133600] ACKed" << COLOR_RESET << endl;
-    cout << BLU << "[i] [Established] [Seg 2] [S=2266135060] ACKed" << COLOR_RESET << endl;
-    cout << YEL << "[i] [Established] [Seg 1] [A=3165500900] Sent" << COLOR_RESET << endl;
-    cout << BLU << "[i] [Established] [Seg 3] [S=2266136520] ACKed" << COLOR_RESET << endl;
-    cout << YEL << "[i] [Established] [Seg 2] [A=3165502360] Sent" << COLOR_RESET << endl;
-    cout << YEL << "[i] [Established] [Seg 3] [A=3165503820] Sent" << COLOR_RESET << endl;
-
-    cout << "[~] [Established] Waiting for segments to be sent" << endl;
-
-
-    // ======== Closing ========
-    char fin_buffer[MAXLINE];
-    auto fin_buffer_size = connection.recv(fin_buffer, MAXLINE, &addr, &len);
-    cout << BLU << "[i] [Closing] Received FIN request from " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
-
-    cout << YEL << "[i] [Closing] Sending FIN-ACK request to " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
-    char fin_ack_buffer[MAXLINE]; ((char*)fin_ack_buffer)[0] = '\0';
-    connection.send((char*)server_ip.c_str(), server_port, (void*)fin_ack_buffer, MAXLINE);
-
-    char ack_buffer_closing[MAXLINE];
-    auto ack_buffer_closing_size = connection.recv(ack_buffer_closing, MAXLINE, &addr, &len);
-    cout << BLU << "[i] [Closing] Received FIN request from " << this->server_ip << ":" << this->server_port << COLOR_RESET << endl;
-
-    cout << GRN << "[i] Connection closed successfully" << COLOR_RESET << endl;
-
-    handleMessage(input_buffer, input_buffer_size);
 }
 
 
--- src/server.cpp
+++ src/server.cpp
@@ -20,56 +20,63 @@ void Server::run() {
     auto sync_buffer_size = connection.recv(&syn_segment, MAXLINE, &addr, &len);
     cout << YEL << "[i] [Handshake] [S=" << syn_segment.seq_num << "] Received SYN request from " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << endl << COLOR_RESET;
 
-    SegmentHandler segment_handler;
-    uint32_t initial_seq_num = segment_handler.generateInitialSeqNum();
-    Segment syn_ack_segment = synAck(syn_segment.seq_num + 1, initial_seq_num);
-retry:
-    cout << BLU << "[i] [Handshake] [A=" << syn_ack_segment.ack_num << "] [S=" << syn_ack_segment.seq_num << "] Sending SYN-ACK request to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
-    connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, &syn_ack_segment, sync_buffer_size);
+    bool retry = true;
+    while (retry) {
+        SegmentHandler segment_handler;
+        uint32_t initial_seq_num = segment_handler.generateInitialSeqNum();
+        Segment syn_ack_segment = synAck(syn_segment.seq_num + 1, initial_seq_num);
+        cout << BLU << "[i] [Handshake] [A=" << syn_ack_segment.ack_num << "] [S=" << syn_ack_segment.seq_num << "] Sending SYN-ACK request to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
+        connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, &syn_ack_segment, sync_buffer_size);
 
-    Segment ack_segment;
-    while (true) {
-        auto ack_buffer_size = connection.recv(&ack_segment, MAXLINE, &addr, &len);
-        if(ack_buffer_size < 0) {
-            cout << RED << "[-] [Handshake] Timeout, retrying" << COLOR_RESET << endl;
-            goto retry;
+        Segment ack_segment;
+        while (true) {
+            auto ack_buffer_size = connection.recv(&ack_segment, MAXLINE, &addr, &len);
+            if(ack_buffer_size < 0) {
+                cout << RED << "[-] [Handshake] Timeout, retrying" << COLOR_RESET << endl;
+                retry = true;
+                break;
+            }
+            retry = false;
+            if(extract_flags(ack_segment.flags) == ACK_FLAG && ack_segment.ack_num == syn_ack_segment.ack_num + 1) break;   
         }
-        if(extract_flags(ack_segment.flags) == ACK_FLAG && ack_segment.ack_num == syn_ack_segment.ack_num + 1) break;   
+        if(retry) continue;
+        
+        cout << YEL << "[i] [Handshake] [A=" << ack_segment.ack_num << "] Received ACK request from " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
+        
+
+        
+        // ======== Established ========
+        cout << "[i] Sending input to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << endl;
+
+        connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, (void*)response_buffer.c_str(), response_buffer.size());
+        cout << BLU << "[i] [Established] [Seg 1] [S=2266133600] Sent" << COLOR_RESET << endl;
+        cout << BLU << "[i] [Established] [Seg 2] [S=2266135060] Sent" << COLOR_RESET << endl;
+        cout << BLU << "[i] [Established] [Seg 3] [S=2266136520] Sent" << COLOR_RESET << endl;
+
+
+        cout << "[~] [Established] Waiting for segments to be ACKed" << endl;
+
+        cout << YEL << "[i] [Established] [Seg 1] [A=3165500900] ACKed" << COLOR_RESET << endl;
+        cout << YEL << "[i] [Established] [Seg 2] [A=3165502360] ACKed" << COLOR_RESET << endl;
+        cout << YEL << "[i] [Established] [Seg 3] [A=3165503820] ACKed" << COLOR_RESET << endl;
+
+
+        // ======== Closing ========
+        cout << YEL << "[i] [Closing] Sending FIN request to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
+        char fin_buffer[MAXLINE]; ((char*)fin_buffer)[0] = '\0';
+        connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, (void*)fin_buffer, sync_buffer_size);
+
+        char fin_ack_buffer[MAXLINE];
+        auto fin_ack_buffer_size = connection.recv(fin_ack_buffer, MAXLINE, &addr, &len);
+        cout << BLU << "[+] [Closing] Received FIN-ACK request from " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
+
+        cout << BLU << "[i] [Closing] Sending ACK request to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
+        char ack_buffer_closing[MAXLINE]; ((char*)ack_buffer_closing)[0] = '\0';
+        connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, (void*)ack_buffer_closing, sync_buffer_size);
+
+        cout << GRN << "[i] Connection closed successfully" << COLOR_RESET << endl;
     }
-    cout << YEL << "[i] [Handshake] [A=" << ack_segment.ack_num << "] Received ACK request from " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
     
-
-    
-    // ======== Established ========
-    cout << "[i] Sending input to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << endl;
-
-    connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, (void*)response_buffer.c_str(), response_buffer.size());
-    cout << BLU << "[i] [Established] [Seg 1] [S=2266133600] Sent" << COLOR_RESET << endl;
-    cout << BLU << "[i] [Established] [Seg 2] [S=2266135060] Sent" << COLOR_RESET << endl;
-    cout << BLU << "[i] [Established] [Seg 3] [S=2266136520] Sent" << COLOR_RESET << endl;
-
-
-    cout << "[~] [Established] Waiting for segments to be ACKed" << endl;
-
-    cout << YEL << "[i] [Established] [Seg 1] [A=3165500900] ACKed" << COLOR_RESET << endl;
-    cout << YEL << "[i] [Established] [Seg 2] [A=3165502360] ACKed" << COLOR_RESET << endl;
-    cout << YEL << "[i] [Established] [Seg 3] [A=3165503820] ACKed" << COLOR_RESET << endl;
-
-
-    // ======== Closing ========
-    cout << YEL << "[i] [Closing] Sending FIN request to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
-    char fin_buffer[MAXLINE]; ((char*)fin_buffer)[0] = '\0';
-    connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, (void*)fin_buffer, sync_buffer_size);
-
-    char fin_ack_buffer[MAXLINE];
-    auto fin_ack_buffer_size = connection.recv(fin_ack_buffer, MAXLINE, &addr, &len);
-    cout << BLU << "[+] [Closing] Received FIN-ACK request from " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
-
-    cout << BLU << "[i] [Closing] Sending ACK request to " << inet_ntoa(addr.sin_addr) << ":" << addr.sin_port << COLOR_RESET << endl;
-    char ack_buffer_closing[MAXLINE]; ((char*)ack_buffer_closing)[0] = '\0';
-    connection.send(inet_ntoa(addr.sin_addr), addr.sin_port, (void*)ack_buffer_closing, sync_buffer_size);
-
-    cout << GRN << "[i] Connection closed successfully" << COLOR_RESET << endl;
 }
 
 
